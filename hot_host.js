b09ceedbd90ebb83d8dee2ece152f4fbdb35779b
Logan Shire <logan.shire@gmail.com>
2026-09-19 16:44:20 -0400

    Publish web client 2026-09-19T20:44:20Z from visor 7857a68

--- hot_host.js
+++ hot_host.js
@@ -17,9 +17,9 @@
 // (dependency lookups answer "absent"); a v3 reactor that never touches
 // `dependencies[...]` runs exactly as it does under a host without DI.
 
-import { BlobWriter, Runtime, Tags, Types, decoder as ffiDecoder, encodeErrorBlob, foreignObjects, pendingCalls, registerForeign, wasiShim } from "./swift_ffi_runtime.js?v=1490878299";
-import { createReactTreeRenderer } from "./runtime/react_renderer.js?v=1490878299";
-import { applyPatch } from "./runtime/flat_tree.js?v=1490878299";
+import { BlobWriter, Runtime, Tags, Types, decoder as ffiDecoder, encodeErrorBlob, foreignObjects, pendingCalls, registerForeign, wasiShim } from "./swift_ffi_runtime.js?v=2610331501";
+import { createReactTreeRenderer } from "./runtime/react_renderer.js?v=2610331501";
+import { applyPatch } from "./runtime/flat_tree.js?v=2610331501";
 
 const decoder = new TextDecoder();
 const encoder = new TextEncoder();
