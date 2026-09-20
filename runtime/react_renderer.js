d373e63aa902275b70696bb0948de949fe93cf56
Logan Shire <logan.shire@gmail.com>
2026-09-20 12:37:53 -0400

    Publish web client 2026-09-20T16:37:53Z from visor 7c74401

--- runtime/react_renderer.js
+++ runtime/react_renderer.js
@@ -12,7 +12,7 @@
 // Uses the React 18 UMD globals (window.React / window.ReactDOM), served
 // from the hermetic @react_umd repositories next to this bundle.
 
-import { SYMBOLS } from "./symbols.js?v=1616920045";
+import { SYMBOLS } from "./symbols.js?v=1029170448";
 
 /// An SF Symbol drawn from the portable table as an inline SVG sized to
 /// the text it stands in (an `Image(systemName:)` is a text node carrying
